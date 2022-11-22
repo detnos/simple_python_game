a3f3070cf21f5241e0843d4d13324a30896a5dca
Hugh <det.nosnibor00@gmail.com>
2022-11-22 21:16:56 +0000

    cleanup aisle 1

--- simplegame.py
+++ simplegame.py
@@ -1,38 +1,36 @@
+from ast import Num
+from multiprocessing import current_process
+
+
 print('Hello welcome to trivia!')
 
-ans = input('Are you ready to play? (yes/no): ')
+play_yn = input('Are you ready to play? (yes/no): ')
 
 score = 0
+num_q = 0
 total_q = 4
+questions_and_answers = [
+    ['What is the best programming language?: ', ['python']],
+    ['What is 2 + 8 + 9 - 1: ', ['18']],
+    ['What color is the sky?: ', ['blue']],
+    ['Who came second in the stanley cup finals?: ', ['nights', 'vegas']]
+]
 
-if ans.lower() == 'yes':
-    ans = input('1. What is the best programming language?: ')
-    if ans.lower() == 'python':
-        score += 1
-        print('Correct')
-    else:
-        print('Incorrect')
-
-    ans = input('2. What is 2 + 8 + 9 - 1: ')
-    if ans == '18':
-        score += 1
-        print('Correct')
-    else:
-        print('Incorrect')
-    
-    ans = input('3. What color is the sky?: ')
-    if ans.lower() == 'blue':
-        score += 1
-        print('Correct')
-    else:
-        print('Incorrect')
-
-    ans = input('4. Who came second in the stanley cup finals?: ')
-    if ans.lower() == 'nights' or ans.lower() == 'vegas':
-        score += 1
-        print('Correct')
-    else:
-        print('Incorrect')
+if play_yn.lower() == 'yes':
+    while num_q < total_q:
+        current_question = questions_and_answers[num_q][0]
+        current_answer = questions_and_answers[num_q][1]
+        num_q +=1
+        prompt = '%s. %s' %(num_q, current_question)
+        ans = input(prompt)
+        print(len(current_answer))
+        for answer in current_answer:
+            if ans.lower() == answer:
+                score += 1
+                print('Correct')
+                break
+            elif answer == current_answer[-1]:
+                print('Incorrect')
 
     print('Thank you for playing, you got', score, 'questions correct.')
     mark = (score / total_q) * 100
